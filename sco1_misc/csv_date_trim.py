368b5580343f5de75fb4f1c7088f32b5f7ea8e79
S. Co1 <sco1.git@gmail.com>
2022-06-21 15:24:35 -0400

    Add `trim_cli` UI flow tests
    * Move trimming logic into its own function for simpler testability
    * Fix typo in trim CLI output filename specifier

--- sco1_misc/csv_date_trim.py
+++ sco1_misc/csv_date_trim.py
@@ -24,48 +24,7 @@ def _build_output_filepath(
     return log_filepath.parent / out_filename
 
 
-@click.command()
-@click.option("--log_filepath", type=Path, help="Path to log file to trim. Leave blank to prompt.")
-@click.option(
-    "--test_date",
-    type=str,
-    default=TODAY_DATESTR,
-    help="Trim date selection, as YYYY-MM-DD. Defaults to today's date.",
-    callback=_date_convert,
-)
-@click.option(
-    "--out-filename", type=str, help="Output filename. Defaults to <in_filename>_<test_date>.csv"
-)
-def trim_cli(
-    log_filepath: Path | None,
-    test_date: dt.date,
-    out_filename: str | None,
-) -> None:
-    """
-    Trim the provided log CSV to rows logged on the specified date.
-
-    The trimmed log file is saved as a separate CSV file alongside the input log file. Any existing
-    trimmed data matching `--out-filename` will be discarded. Attempting to overwrite the input CSV
-    file will abort the process.
-
-    It is assumed that the CSV file contains a column named `"Time"`, with timestamps
-    formatted as `MM/DD/YYYY HH:MM:SS`. It is also assumed that the CSV file ends on the same date
-    as the specified date filter.
-    """
-    if log_filepath is None:
-        try:
-            log_filepath = prompt_for_file(
-                "Select Log File.",
-                filetypes=[("Log CSV", ".csv"), ("All Files", "*.*")],
-            )
-        except ValueError as err:
-            # Reraise as Click exception to gracefully exit
-            raise click.ClickException("No file selected, aborting.") from err
-
-    out_filepath = _build_output_filepath(log_filepath, test_date, out_filename)
-    if out_filepath == log_filepath:
-        raise click.ClickException("Cannot overwrite the input log file. Aborting.")
-
+def _trim_csv(log_filepath: Path, out_filepath: Path, test_date: dt.date) -> None:
     with log_filepath.open("r") as in_log, out_filepath.open("w", newline="") as out_log:
         reader = csv.DictReader(in_log)
         if not reader.fieldnames:
@@ -82,6 +41,51 @@ def trim_cli(
                 # we've hit the first correct date, we can just dump the rest of the rows
                 writer.writerow(row)  # Write the current row since we've consumed it already
                 writer.writerows(reader)
+
+
+@click.command()
+@click.option("--log_filepath", type=Path, help="Path to log file to trim. Leave blank to prompt.")
+@click.option(
+    "--test_date",
+    type=str,
+    default=TODAY_DATESTR,
+    help="Trim date selection, as YYYY-MM-DD. Defaults to today's date.",
+    callback=_date_convert,
+)
+@click.option(
+    "--out_filename", type=str, help="Output filename. Defaults to <in_filename>_<test_date>.csv"
+)
+def trim_cli(
+    log_filepath: Path | None,
+    test_date: dt.date,
+    out_filename: str | None,
+) -> None:
+    """
+    Trim the provided log CSV to rows logged on the specified date.
+
+    The trimmed log file is saved as a separate CSV file alongside the input log file. Any existing
+    trimmed data matching `--out_filename` will be discarded. Attempting to overwrite the input CSV
+    file will abort the process.
+
+    It is assumed that the CSV file contains a column named `"Time"`, with timestamps
+    formatted as `MM/DD/YYYY HH:MM:SS`. It is also assumed that the CSV file ends on the same date
+    as the specified date filter.
+    """
+    if log_filepath is None:
+        try:
+            log_filepath = prompt_for_file(
+                "Select Log File",
+                filetypes=[("Log CSV", ".csv"), ("All Files", "*.*")],
+            )
+        except ValueError as err:
+            # Reraise as Click exception to gracefully exit
+            raise click.ClickException("No file selected, aborting.") from err
+
+    out_filepath = _build_output_filepath(log_filepath, test_date, out_filename)
+    if out_filepath == log_filepath:
+        raise click.ClickException("Cannot overwrite the input log file. Aborting.")
+
+    _trim_csv(log_filepath, out_filepath, test_date)
 
 
 if __name__ == "__main__":  # pragma: no cover
--- tests/test_csv_date_trim.py
+++ tests/test_csv_date_trim.py
@@ -1,5 +1,8 @@
 import datetime as dt
 from pathlib import Path
+from unittest import mock
+
+from click.testing import CliRunner
 
 from sco1_misc import csv_date_trim
 
@@ -21,3 +24,46 @@ def test_date_convert_filename() -> None:
     )
 
     assert out_filepath == CURRENT_DIR / "taco_bell_4lyfe.csv"
+
+
+def test_same_path_raises() -> None:
+    runner = CliRunner()
+    result = runner.invoke(
+        csv_date_trim.trim_cli, ["--log_filepath", "some_log.csv", "--out_filename", "some_log.csv"]
+    )
+
+    assert result.exit_code == 1
+
+
+@mock.patch.object(csv_date_trim, "prompt_for_file", return_value=DUMMY_LOG_FILEPATH)
+@mock.patch.object(csv_date_trim, "_trim_csv")
+def test_no_log_path_prompts(mocked_trim: mock.MagicMock, mocked_prompt: mock.MagicMock) -> None:
+    runner = CliRunner()
+    result = runner.invoke(csv_date_trim.trim_cli)
+
+    assert result.exit_code == 0
+    mocked_prompt.assert_called()
+    mocked_trim.assert_called()
+
+
+@mock.patch.object(csv_date_trim, "prompt_for_file", side_effect=ValueError)
+@mock.patch.object(csv_date_trim, "_trim_csv")
+def test_no_log_path_prompt_selection_raises(
+    mocked_trim: mock.MagicMock,
+    mocked_prompt: mock.MagicMock,
+) -> None:
+    runner = CliRunner()
+    result = runner.invoke(csv_date_trim.trim_cli)
+
+    assert result.exit_code == 1
+    mocked_prompt.assert_called()
+    mocked_trim._trim_csv.assert_not_called()
+
+
+@mock.patch.object(csv_date_trim, "_trim_csv")
+def test_trim_pipeline_invoked(mocked_trim: mock.MagicMock) -> None:
+    runner = CliRunner()
+    result = runner.invoke(csv_date_trim.trim_cli, ["--log_filepath", "some_log.csv"])
+
+    assert result.exit_code == 0
+    mocked_trim.assert_called()
